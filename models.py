ab9d34aaeb3fa6850f231dfec9bf284793c10243
kaitoml <rs9992112@gmail.com>
2026-09-19 00:02:19 +0400

    Added run_autotraining method to Sequential model

--- models.py
+++ models.py
@@ -39,3 +39,29 @@ class Sequential(Model):
         for parameter in self.parameters():
             parameter.requires_grad = False
 
+    def run_autotraining(self, epochs, optimizer, criterion, x_train, y_train, x_val, y_val):
+        from tqdm.auto import tqdm
+
+        train_losses = []
+        val_losses = []
+
+        loop = tqdm(range(epochs), total=epochs, desc='Training')
+        for _ in loop:
+            # ------- TRAIN ------
+            self.train()
+            y_pred_logits = self(x_train)
+            loss = criterion(y_pred_logits, y_train)
+            train_losses.append(loss.data)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.update()
+
+            # ------ EVAL ------
+            self.evaluate()
+            y_pred_logits = self(x_val)
+            loss = criterion(y_pred_logits, y_val)
+            val_losses.append(loss.data)
+            loop.set_postfix(test_loss=f'{loss.data}')
+
+        return train_losses, val_losses
+
